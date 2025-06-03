bd9f35e87f96a5966e6c2889ba2e66e890f2fc36
Darkstalker17 <dakshithkarthik.a@gmail.com>
2025-06-03 14:35:07 +0400

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,9 @@
+rad = int(input("Enter the radius: "))
+def circumference(r):
+    c = (2*3.1415*r)//1
+    print("Circumference of the circle is", c)
+def area(j):
+    a = (3.1415*j*j)//1
+    print("area of the circle is", a)
+area(rad)
+circumference(rad)
